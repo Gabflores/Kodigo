05e4d73460c02aea486a5a7861d119ac066f590c
aguustin <94425896+aguustin@users.noreply.github.com>
2022-07-06 22:14:01 -0300

    Create listarNotasMaterias.java

---
+++ src/main/java/com/universidad/listarNotasMaterias.java
@@ -0,0 +1,7 @@
+package com.universidad;
+
+public interface listarNotasMaterias {  //se agrego con el unico fin de implementar una interfaz
+     void RegistrarAlumnosNM();
+
+     void ListarAlumnosNM();
+}
